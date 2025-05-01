e638cbeb7d556ee042b3233ec4858a1af434695e
baronclaps <barbarhendo180@gmail.com>
2025-05-01 13:07:17 -0500

    Create Angle + Distance classes

---
+++ src/main/java/com/pedropathing/math/Angle.java
@@ -0,0 +1,7 @@
+package com.pedropathing.math;
+
+public class Angle {
+    public enum Unit {
+        RADIANS, DEGREES
+    }
+}
---
+++ src/main/java/com/pedropathing/math/Distance.java
@@ -0,0 +1,8 @@
+package com.pedropathing.math;
+
+public class Distance {
+
+    enum Unit {
+        MM, CM, M, KM, IN, FT, YD, MI
+    }
+}
