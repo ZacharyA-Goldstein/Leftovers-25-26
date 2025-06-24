3a448f959e9adab24663073c0409a23b7bad6f01
baronclaps <barbarhendo180@gmail.com>
2025-06-24 00:13:00 -0500

    Remove old jdoc creation

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,19 +13,6 @@ plugins {
 	id("org.jetbrains.dokka") version "1.9.20"
 }
 
-tasks.named<org.jetbrains.dokka.gradle.DokkaTask>("dokkaJavadoc") {
-	outputDirectory.set(file("${buildDir}/dokka"))
-
-	dokkaSourceSets {
-		named("main") {
-			sourceRoots.from(file("src/main/kotlin"), file("src/main/java"))
-			reportUndocumented.set(true)
-			skipDeprecated.set(false)
-			jdkVersion.set(8)
-		}
-	}
-}
-
 
 val defaultMinSdkVersion by extra(29)
 val defaultMinSdkVersion1 by extra(23)
@@ -102,12 +89,13 @@ tasks.register<Jar>("sourcesJar") {
 
 dependencies {
 	implementation("androidx.annotation:annotation-jvm:1.9.1")
+	//noinspection Aligned16KB
 	compileOnly("org.firstinspires.ftc:RobotCore:10.2.0")
 	compileOnly("org.firstinspires.ftc:Hardware:10.2.0")
 	compileOnly("org.firstinspires.ftc:FtcCommon:10.2.0")
 	compileOnly("org.firstinspires.ftc:RobotServer:10.2.0")
 	compileOnly("org.firstinspires.ftc:OnBotJava:10.2.0")
-	compileOnly("com.bylazar:ftcontrol:0.6.6")
+	compileOnly("com.bylazar:ftcontrol:0.6.7")
 	implementation("org.apache.commons:commons-math3:3.6.1")
 	dokkaHtmlPlugin("org.jetbrains.dokka:kotlin-as-java-plugin:1.9.20")
 	dokkaGfmPlugin("org.jetbrains.dokka:kotlin-as-java-plugin:1.9.20")
@@ -119,7 +107,7 @@ publishing {
 		register<MavenPublication>("release") {
 			groupId = "com.pedropathing"
 			artifactId = "dev"
-			version = "1.1.0-dev12"
+			version = "1.1.0-dev13"
 
 			afterEvaluate {
 				from(components["release"])
