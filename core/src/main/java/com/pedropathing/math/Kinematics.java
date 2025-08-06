27bb0c7cd1d47d1d3c2415bf415f1b8c09e348f3
baronclaps <barbarhendo180@gmail.com>
2025-08-06 15:30:22 -0500

    Javadoc publishing fixes

--- core/src/main/java/com/pedropathing/math/Kinematics.java
+++ core/src/main/java/com/pedropathing/math/Kinematics.java
@@ -5,13 +5,13 @@ package com.pedropathing.math;
  * Used for decelerating the robot.
  *
  * <pre>
- * v<sub>f</sub><sup>2</sup> = v<sub>i</sub><sup>2</sup> + 2·a·d
+ * vf² = vi² + 2 * a * d
  * </pre>
  * Where:<br>
  * <ul>
- *   <li><b>v<sub>f</sub></b> = final velocity</li>
- *   <li><b>v<sub>i</sub></b> = initial velocity</li>
- *   <li><b>a</b> = acceleration</li> (or deceleration; always negative)
+ *   <li><b>vf</b> = final velocity</li>
+ *   <li><b>vi</b> = initial velocity</li>
+ *   <li><b>a</b> = acceleration (or deceleration; always negative)</li>
  *   <li><b>d</b> = distance (equations arranged in code for positive and negative)</li>
  * </ul>
  *
@@ -23,20 +23,20 @@ public final class Kinematics {
      * Get the final velocity at a given distance with the deceleration.
      *
      * <pre>
-     * v<sub>f</sub><sup>2</sup> = v<sub>i</sub><sup>2</sup> + 2·a·d
+     * vf² = vi² + 2 * a * d
      * Solve for vi
-     * v<sub>i</sub> = sqrt(v<sub>f</sub><sup>2</sup> - 2·a·d)
-     * Set v<sub>f</sub> to 0 to stop
-     * v<sub>i</sub> = sqrt(0 - 2·a·d)
-     * v<sub>i</sub> = sqrt(-2·a·d)
+     * vi = sqrt(vf² - 2 * a * d)
+     * Set vf to 0 to stop
+     * vi = sqrt(0 - 2 * a * d)
+     * vi = sqrt(-2 * a * d)
      * </pre>
      */
     public static double getVelocityToStopWithDeceleration(
-        double directionalDistance,
-        double deceleration
+            double directionalDistance,
+            double deceleration
     ) {
         return Math.signum(directionalDistance)
-            * Math.sqrt(Math.abs(-2 * deceleration * directionalDistance));
+                * Math.sqrt(Math.abs(-2 * deceleration * directionalDistance));
     }
 
     /**
@@ -44,25 +44,25 @@ public final class Kinematics {
      * as it decelerates.
      *
      * <pre>
-     * v<sub>f</sub><sup>2</sup> = v<sub>i</sub><sup>2</sup> + 2·a·d
-     * Solve for v<sub>f</sub>
-     * v<sub>f</sub> = sqrt(v<sub>i</sub><sup>2</sup> + 2·a·d)
+     * vf² = vi² + 2 * a * d
+     * Solve for vf
+     * vf = sqrt(vi² + 2 * a * d)
      * </pre>
      */
     public static double getFinalVelocityAtDistance(
-        double currentVelocity,
-        double deceleration,
-        double directionalDistance
+            double currentVelocity,
+            double deceleration,
+            double directionalDistance
     ) {
         return Math.signum(directionalDistance) * Math.sqrt(Math.abs(currentVelocity * currentVelocity
-            + 2 * deceleration * Math.abs(directionalDistance)));
+                + 2 * deceleration * Math.abs(directionalDistance)));
     }
 
     /**
      * Predict the next loop's velocity.
      *
      * <pre>
-     * v<sub>next</sub> = 2·v<sub>current</sub> - v<sub>previous</sub>
+     * v_next = 2 * v_current - v_previous
      * </pre>
      */
     public static double predictNextLoopVelocity(double currentVelocity, double previousVelocity) {
@@ -73,18 +73,18 @@ public final class Kinematics {
      * Calculate the distance needed to reach a velocity with the deceleration.
      *
      * <pre>
-     * v<sub>f</sub><sup>2</sup> = v<sub>i</sub><sup>2</sup> + 2·a·d
+     * vf² = vi² + 2 * a * d
      * Solve for d
-     * d = -(v<sub>f</sub><sup>2</sup> - v<sub>i</sub><sup>2</sup>) / (2·a)
+     * d = -(vf² - vi²) / (2 * a)
      * </pre>
      *
      * @param velocity Negative or positive.
      * @param deceleration Negative.
      */
     public static double getDistanceToVelocity(
-        double velocity,
-        double deceleration,
-        double targetVelocity
+            double velocity,
+            double deceleration,
+            double targetVelocity
     ) {
         return (targetVelocity * targetVelocity - velocity * velocity) / (2 * deceleration);
     }
@@ -93,9 +93,9 @@ public final class Kinematics {
      * Calculate the distance needed to stop at a given velocity with the deceleration.
      *
      * <pre>
-     * v<sub>f</sub><sup>2</sup> = v<sub>i</sub><sup>2</sup> + 2·a·d
+     * vf² = vi² + 2 * a * d
      * Solve for d
-     * d = -(v<sub>f</sub><sup>2</sup> - v<sub>i</sub><sup>2</sup>) / (2·a)
+     * d = -(vf² - vi²) / (2 * a)
      * distanceToTarget > d
      * </pre>
      *
@@ -103,8 +103,8 @@ public final class Kinematics {
      * @param deceleration Negative.
      */
     public static double getStoppingDistance(
-        double velocity,
-        double deceleration
+            double velocity,
+            double deceleration
     ) {
         return getDistanceToVelocity(velocity, deceleration, 0);
     }
