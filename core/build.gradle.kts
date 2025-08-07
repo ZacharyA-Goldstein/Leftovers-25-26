185a4679adda3fc2ad643f65a686556d92c2468a
baronclaps <barbarhendo180@gmail.com>
2025-08-07 16:04:36 -0500

    Panels updates

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -7,7 +7,7 @@ dependencies {
 }
 
 java {
-    toolchain.languageVersion.set(JavaLanguageVersion.of(21))
+    toolchain.languageVersion.set(JavaLanguageVersion.of(17))
     withSourcesJar()
     withJavadocJar()
 }
