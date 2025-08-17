bc4a3b5eeaf5c48be5128919f533e912035288b8
junkjunk123 <havitechrobo@gmail.com>
2025-08-16 22:08:56 -0700

    drive pid

--- core/src/main/java/com/pedropathing/control/FilteredPIDFCoefficients.java
+++ core/src/main/java/com/pedropathing/control/FilteredPIDFCoefficients.java
@@ -66,6 +66,17 @@ public class FilteredPIDFCoefficients {
         T = t;
     }
 
+    /**
+     * This returns the coefficient for the feedforward factor.
+     *
+     * @param input this is inputted into the feedforward equation, if applicable. If there's no
+     *              equation, then any input can be used.
+     * @return This returns the coefficient for the feedforward factor.
+     */
+    public double getCoefficient(double input) {
+        return F.getConstant(input);
+    }
+
     @Override
     public String toString() {
         return "P: " + P + ", I: " + I + ", D: " + D + ", T: " + T + ", F: " + F;
