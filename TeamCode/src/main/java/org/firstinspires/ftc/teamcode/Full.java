82aa9ebe9612181b4b6683d95d7dd2a6a2aa70bc
ZacharyA-Goldstein <zgoldstein27@klschools.org>
2025-10-28 09:30:16 -0400

    Full code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Full.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Full.java
@@ -4,7 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
-
+//full
 @TeleOp(name = "Full", group = "TeleOp")
 public class Full extends OpMode {
 
