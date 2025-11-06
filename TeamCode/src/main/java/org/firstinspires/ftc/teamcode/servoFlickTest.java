999355c3e4520acf346d001816f57eb24f972bd2
rpatel27 <rpatel27@klschools.org>
2025-11-06 09:04:06 -0500

    servo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/servoFlickTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/servoFlickTest.java
@@ -4,7 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Servo;
 
-@TeleOp(name = "ServoFlickTeleOp", group = "Examples")
+@TeleOp(name = "ServoFlickTest", group = "TeleOp")
 public class servoFlickTest extends OpMode {
 
     private Servo servo;
